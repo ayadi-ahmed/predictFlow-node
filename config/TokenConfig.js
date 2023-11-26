55ccb5b36e619f9ce1a842d42b8f80f96a090ca7
Ahmed Ayadi <ayadi.ahmed4@gmail.com>
2023-11-26 10:07:00 +0100

    login and register added

---
+++ config/TokenConfig.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+function verifyToken(req, res, next) {
+  if(req.headers.authorization==null)
+    return  res.status(401).send("you should authenticated first");
+  const token = req.headers.authorization.split(' ')[1];
+  const secret = process.env.JWT_SECRET_KEY;
+
+  jwt.verify(token, secret, function(err, decoded) {
+    if (err) {
+      res.sendStatus(401);
+    } else {
+      req.userId = decoded._id;
+      next();
+    }
+  });
+}
+
+module.exports=verifyToken;
--- controller/user.js
+++ controller/user.js
@@ -2,9 +2,89 @@ const { UserService } = require("../services");
 const errorHandler = require("./error-handler");
 cosnt = require("./error-handler")
 
+
+
+const express = require('express');
+const router = express.Router();
+const user = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt =  require('jsonwebtoken');
+const verifyToken = require("/Users/ahmed_ayadi/Desktop/TEKUP/projet-sem1/predictistock_project/config/tokenConfig");
+require('dotenv').config();
+
+
+
+
 module.exports = (app) => {
 
     const service = new UserService();
+
+    //register user
+/*     router.post('/register',async(req,res,next)=>{
+        try {
+            //let usr = await service.create(req.body);
+            usr = new user(req.body);
+            salt = bcrypt.genSaltSync(10);
+            bcryptPassword = bcrypt.hashSync(req.body.password,salt);
+            usr.password = bcryptPassword;
+            result = await usr.save();
+            return res.status(200).send({ message: "user created successfully", data: usr });     
+        } catch (error) {
+            return res.status(400).send(error);
+        }
+}) */
+
+     app.post("/register", async (req, res, next) => {
+        try {
+            //let user = await service.create(req.body);
+            let usr = new user(req.body);
+            
+            salt = bcrypt.genSaltSync(10);
+            bcryptPassword = await bcrypt.hashSync(req.body.password,salt);
+            console.log("pass non hash "+ usr.password + " pass hashé "+bcryptPassword);
+            usr.password = bcryptPassword;
+            console.log("user "+ usr);
+            await usr.save();
+            res.status(200).send({ message: "user created successfully", data: usr })
+        } catch (error) {
+            next(error)
+        }
+    }) 
+
+app.post('/login',async(req,res)=>{
+
+    let jwtSecretKey = process.env.JWT_SECRET_KEY; 
+    data=req.body;
+    console.log("data "+JSON.stringify(data));
+    let usr = await user.findOne({email:req.body.email});
+    console.log("usr "+usr);
+    
+    if(!usr){ 
+        res.status(404).send("email or password invalid");
+    }else{
+        validPass= bcrypt.compareSync(req.body.password, usr.password)
+        if(!validPass){
+            res.status(401).send('email or password invalid');
+        }else{
+            payload={
+                _id:usr._id,
+                email:usr.email,
+                name:usr.name,
+                phoneNumber:usr.phoneNumber,
+                articles:usr.articles,
+                time: Date(), 
+            }
+            token=jwt.sign(payload,jwtSecretKey);
+            res.status(200).send({token});
+        }
+    }
+})
+
+
+
+
+
+
 
     app.get("/user", async (req, res, next) => {
         try {
@@ -36,14 +116,7 @@ module.exports = (app) => {
         }
     })
 
-    app.post("/user", async (req, res, next) => {
-        try {
-            const user = await service.create(req.body);
-            res.status(200).send({ message: "user created successfully", data: user })
-        } catch (error) {
-            next(error)
-        }
-    })
+
 
     app.delete("/user/:id", async (req, res, next) => {
         try {
